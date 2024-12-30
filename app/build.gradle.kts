c2841b2feb7a1d16dd0ea4e9a5c5c80985683f5d
Revalinazwa <119474745+Revalinazwa@users.noreply.github.com>
2024-12-30 09:57:02 +0700

    update version 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "uas.pam.budgetnotes2"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
